9cd600870449b830988cb1045ec792b58e67731c
Tom <squid@stormwind.it>
2022-09-25 19:23:34 +0200

    TypesSelect

--- src/diceclub-app/src/components/cards/types.select.tsx
+++ src/diceclub-app/src/components/cards/types.select.tsx
@@ -3,3 +3,7 @@ import { observer } from "mobx-react";
 import { useStore } from "../../stores/store.context";
 
 import { Select } from "semantic-ui-react";
+
+export const TypesSelectComponent = observer(() => {
+  return <></>;
+});
