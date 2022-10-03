46bf4a095b99934aeb86801a28e1048f4d1f21b9
Tom <tom@isendu.com>
2022-10-03 16:14:23 +0200

    Updated

--- src/diceclub-app/src/api_client/api.config.ts
+++ src/diceclub-app/src/api_client/api.config.ts
@@ -1,5 +1,7 @@
 export const apiConfig = {
 	timeout: 30000,
-	baseURL: `${import.meta.env.BASE_API_URL || "http://localhost:5280"}`,
-	defaultPageSize: 30
+	baseURL: `${
+		import.meta.env.BASE_API_URL || `${window.location.origin.toString()}`
+	}`,
+	defaultPageSize: 30,
 };
