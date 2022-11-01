77e3ceb163b071a7828664c3aacf68f5821c760a
tgiachi <squid@stormwind.it>
2022-11-01 20:39:14 +0100

    up

--- src/diceclub-image-resizer/.projenrc.js
+++ src/diceclub-image-resizer/.projenrc.js
@@ -3,9 +3,17 @@ const project = new typescript.TypeScriptAppProject({
   defaultReleaseBranch: 'alpha',
   name: 'diceclub-image-resizer',
 
-  deps: ['p-map@4.0.0', 'sharp'] /* Runtime dependencies of this module. */,
+  deps: [
+    'p-map@4.0.0',
+    'sharp',
+    'node-tesseract-ocr',
+    'fs-extra',
+  ] /* Runtime dependencies of this module. */,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: ['@types/sharp'] /* Build dependencies for this module. */,
+  devDeps: [
+    '@types/sharp',
+    '@types/fs-extra',
+  ] /* Build dependencies for this module. */,
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
--- src/diceclub-image-resizer/src/index.ts
+++ src/diceclub-image-resizer/src/index.ts
@@ -1,17 +1,36 @@
 import fs from 'fs';
 import path from 'path';
+
+import tesseract from 'node-tesseract-ocr';
+
 import pMap from 'p-map';
-
 import sharp from 'sharp';
-
-const resizeFile = async (filename: string): Promise<void> => {
+let files: string[] = [];
+//const contrast = 1.5;
+//const brightness = 0.5;
+const resizeFile = async (filename: string, index: number): Promise<void> => {
   if (filename.endsWith('.jpg')) {
     const filePath = path.join(baseDirectoryPath, filename);
-    const resizedFilePath = path.join(resizedDirectoryPath, filename);
-    console.log(`Resizing ${filePath} to ${resizedFilePath}`);
+    const resizedFilePath = path.join(resizedDirectoryPath, `${index}.jpg`);
+    console.log(
+      `[${index}/${files.length}]] Resizing ${filePath} to ${resizedFilePath}`,
+    );
 
     const image = sharp(filePath);
-    await image.resize(370, 546).toFile(resizedFilePath);
+    await image
+      .resize(370, 546)
+      .extract({ left: 15, top: 10, width: 295, height: 50 })
+      .greyscale()
+    //.linear(contrast, (128 * contrast) + 128)
+    //.modulate({ brightness: brightness })
+      .toFile(resizedFilePath);
+
+    const test = await tesseract.recognize(resizedFilePath, {
+      lang: 'ita',
+      oem: 1,
+      psm: 3,
+    });
+    console.log(`[${index}/${files.length}]] ${test}`);
   }
 };
 
@@ -24,10 +43,10 @@ const resizedDirectoryPath = path.join(baseDirectoryPath, 'resized');
 console.log('baseDirectoryPath', baseDirectoryPath);
 console.log('resizedDirectoryPath', resizedDirectoryPath);
 
-const files: string[] = [];
 fs.readdirSync(baseDirectoryPath).forEach((file) => files.push(file));
 
 console.log('files', files.length);
+files = files.slice(0, 300);
 
 void (async () => {
   await pMap(files, resizeFile, { concurrency: 10 });
